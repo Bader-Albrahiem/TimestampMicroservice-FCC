6522e1f6d39343d15d7c03274c7ed3463a586c81
bader-albrahiem <balbrahiem@outlook.com>
2023-12-06 18:38:33 +0300

    intial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/Views/index.html");
+});
+
+app.get("/api/:date", (req, res) => {
+  const date = req.params.date;
+  let result = {};
+
+  if (!isNaN(date)) {
+    const unixTimestamp = parseInt(date);
+    result = {
+      unix: unixTimestamp,
+      utc: new Date(unixTimestamp).toUTCString(),
+    };
+  } else {
+    const dateObject = new Date(date);
+    if (dateObject.toString() === "Invalid Date") {
+      result = { error: "Invalid Date" };
+    } else {
+      result = {
+        unix: dateObject.getTime(),
+        utc: dateObject.toUTCString(),
+      };
+    }
+  }
+
+  res.send(result);
+});
+app.listen(port, () => {
+  console.log(`Server is running in port: ${port}`);
+});
